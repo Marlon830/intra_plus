b01ddbe6372b71dd8c20564310877a0571d58cc3
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-02-06 10:12:25 +0100

    initial commit

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("button1").addEventListener("click", function(){
+        var body = document.getElementsByTagName("body")[0].style.backgroundColor = "green";
+    });
+});
