86c38618ec29f3754f38089037a52da0af4d7de1
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-02-08 15:24:26 +0100

    added bootstrap too

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,5 @@
 var nb_of_kick_off = 0;
+var nb_of_bootstrap = 0;
 
 document.addEventListener("DOMContentLoaded", function(){
     chrome.storage.local.get(['key'], function(result) {
@@ -12,13 +13,20 @@ document.addEventListener("DOMContentLoaded", function(){
                 if (allObjects[i].type.includes("Kick-off")) {
                     nb_of_kick_off++;
                     var par = document.createElement("p");
-                    var text = document.createTextNode(allObjects[i].name);
+                    var text = document.createTextNode(allObjects[i].name + ", you can register it until " + allObjects[i].dateStart);
+                    par.appendChild(text);
+                    document.body.appendChild(par);
+                }
+                if (allObjects[i].type.includes("Bootstrap")) {
+                    nb_of_bootstrap++;
+                    var par = document.createElement("p");
+                    var text = document.createTextNode(allObjects[i].name + ", you can register it until " + allObjects[i].dateStart);
                     par.appendChild(text);
                     document.body.appendChild(par);
                 }
             }
         }
-        if (nb_of_kick_off == 0) {
+        if (nb_of_kick_off == 0 && nb_of_bootstrap == 0) {
             var par = document.createElement("h2");
             var text = document.createTextNode("You are registered to everything!");
             par.appendChild(text);
