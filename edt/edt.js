38cf39cc85062f93a7336c1830221fab31514cab
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-02-11 20:29:58 +0100

    added better code, now edt.js is clean

--- edt/edt.js
+++ edt/edt.js
@@ -1,41 +1,24 @@
+function changeColor(module, color, allCells, i)
+{
+    if (allCells[i].innerText.includes(module)) {
+        allCells[i].style.backgroundColor = color;
+        allCells[i].style.borderColor= color;
+        allCells[i].getElementsByTagName("h4")[0].style.backgroundColor = color;
+    }
+}
+
 function myFunction() {
     var allCells = document.getElementsByClassName("appoint");
     for (var i = 0; i != allCells.length; i++) {
-        if (allCells[i].innerText.includes("G0 - Free Coaching")) {
-            allCells[i].style.backgroundColor = "#FFC857";
-            allCells[i].style.borderColor= "#FFC857";
-            allCells[i].getElementsByTagName("h4")[0].style.backgroundColor = "#FFC857";
-        }
-        if (allCells[i].innerText.includes("B2 -") || allCells[i].innerText.includes("Workshop")) {
-            allCells[i].style.backgroundColor = "#912F40";
-            allCells[i].style.borderColor= "#912F40";
-            allCells[i].getElementsByTagName("h4")[0].style.backgroundColor = "#912F40";
-        }
-        if (allCells[i].innerText.includes("G0 - Extra Curricular Engagement")) {
-            allCells[i].style.backgroundColor = "#702632";
-            allCells[i].style.borderColor= "#702632";
-            allCells[i].getElementsByTagName("h4")[0].style.backgroundColor = "#702632";
-        }
-        if (allCells[i].innerText.includes("B0 - Hub")) {
-            allCells[i].style.backgroundColor = "#40434E";
-            allCells[i].style.borderColor= "#40434E";
-            allCells[i].getElementsByTagName("h4")[0].style.backgroundColor = "#40434E";
-        }
-        if (allCells[i].innerText.includes("B1 -")) {
-            allCells[i].style.backgroundColor = "#080705";
-            allCells[i].style.borderColor= "#080705";
-            allCells[i].getElementsByTagName("h4")[0].style.backgroundColor = "#080705";
-        }
-        if (allCells[i].innerText.includes("B0 - English")) {
-            allCells[i].style.backgroundColor = "#C3DFE0";
-            allCells[i].style.borderColor= "#C3DFE0";
-            allCells[i].getElementsByTagName("h4")[0].style.backgroundColor = "#C3DFE0";
-        }
-        if (allCells[i].innerText.includes("G0 - Epitech") || allCells[i].innerText.includes("G0 - Promoting")) {
-            allCells[i].style.backgroundColor = "#C4B7CB";
-            allCells[i].style.borderColor= "#C4B7CB";
-            allCells[i].getElementsByTagName("h4")[0].style.backgroundColor = "#C4B7CB";
-        }
+        changeColor("G0 - Free Coaching", "#FFC857", allCells, i);
+        changeColor("B2 -", "#912F40", allCells, i);
+        changeColor("Workshop", "#912F40", allCells, i);
+        changeColor("G0 - Epitech", "#C4B7CB", allCells, i);
+        changeColor("G0 - Promoting", "#C4B7CB", allCells, i);
+        changeColor("B0 - English", "#C3DFE0", allCells, i);
+        changeColor("B1 -", "#080705", allCells, i);
+        changeColor("B0 - Hub", "#40434E", allCells, i);
+        changeColor("G0 - Extra Curricular Engagement", "#702632", allCells, i);
     }
     setTimeout(myFunction, 2000);
 }
